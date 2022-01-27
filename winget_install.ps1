d7821c79377e0189740024acb8355e1e77ecf27d
j-archit <archit.jain38@gmail.com>
2022-01-27 18:06:41 +0530

    Improved Flexibility of winget installer Script

--- winget_install.ps1
+++ winget_install.ps1
@@ -1,62 +1,18 @@
-$Log = ".\installer.log"
-$NotInstalledString = "No installed package found matching input criteria."
-
-function Install-Package-ScriptBlock {
-    param (
-        [string]$Package, 
-        [string]$Source,
-        [hashtable]$PackageParams,
-        [hashtable]$OtherParameters
-    )
-
-    $QueryString = $Package
-
-    if($Source -ne ""){
-        $QueryString = $QueryString + " -s " + $Source
-    }
-    if($OtherParameters -ne $null){
-        ForEach($Option in $OtherParameters.Keys){
-            $QueryString = $QueryString + " " + $Option + " " + $OtherParameters[$Option]
-        }
-    }
-
-    # Check if Already Exists
-    $CheckInstallString = "winget list -q " + $QueryString + "| Out-String -Stream"
-    $scriptBlock = [Scriptblock]::Create($CheckInstallString)
-    $exist = Start-Job -ScriptBlock $scriptBlock
-    Wait-Job $exist
-    $exists = Receive-Job $exist
-
-    if(!$exists.Contains($NotInstalledString)){
-        $LogOp = $Package + " already installed, skipping."
-        $abc = "Write-Output " + $LogO
-        return $abc
-    }
-    else{
-        # Install
-        $InstallString = "winget install " + $QueryString
-        $LogOutput = "Issuing Install of " + $Package + " with command: `r`n" + $InstallString
-        $LogOutput | Out-File -FilePath $Log -Append
-        return $InstallString
-    }
-}
-
-# Define Sources
+# Define Sources, Hashes, Constants and Program Lists in Hashes
 $s1 = "winget"
 $s2 = "msstore"
+
+$NotInstalledString = "No installed package found matching input criteria."
+$Log = ".\installer.log"
+"" | Out-File -FilePath $Log
+
 
 $DefaultOptionsHash = @{
     '--accept-source-agreements'  = ""
     '--accept-package-agreements' = ""
     }
 
-#############################
-# Install Required Programs #
-#############################
-
-Write-Output "Installing Required Programs.. " | Out-File -FilePath $Log -Append
-
-$MainProgramsw = @{
+$MainPrograms = @{
     "vscode"   = $s1
     "Python 3" = $s1
     "Windows Terminal" = $s1
@@ -68,36 +24,136 @@ $MainProgramsw = @{
     "Whatsapp" = $s2
     "git" = $s1
     "NVIDIA GeForce Experience" = $s1
-    "GNU Octave" = $s1
+    "Octave" = $s1
 }
 
-$MainPrograms = @{
-    "Python 3"   = $s1
-    "windows terminal" = $s1
+$SecondaryPrograms = @{
+    "7-Zip" = $s1
+    "libreoffice" = $s1
+    "ModernFlyouts" = $s1
+    "Geogebra Classic" = $s1
 }
+
+$PackageParams = @{
+    "Geogebra Classic" = @{
+                            "--id" = "Geogebra.Classic"
+                            }
+
+}
+
+# Functions
+function Logger{
+    param(
+        [string]$Message
+    )
+    $Message | Out-File -FilePath $Log -Append
+    Write-Host $Message
+}
+
+function Install-Package-ScriptBlock {
+    param (
+        [string]$Package, 
+        [string]$Source,
+        [hashtable]$PackageParams,
+        [hashtable]$OtherParameters
+    )
+
+    $QueryString = "'" + $Package + "'"
+
+    if($Source -ne ""){
+        $QueryString = $QueryString + " -s " + $Source
+    }
+    
+    if($PackageParams -ne $null){
+        ForEach($Option in $PackageParams.Keys){
+            $QueryString = $QueryString + " " + $Option + " " + $OtherParameters[$Option]
+        }
+    }
+
+    # Before Adding Default Parameters, create the CheckInstallString
+    $CheckInstallString = "winget list -q " + $QueryString + "| Out-String -Stream"
+
+    if($OtherParameters -ne $null){
+        ForEach($Option in $OtherParameters.Keys){
+            $QueryString = $QueryString + " " + $Option + " " + $OtherParameters[$Option]
+        }
+    }
+
+    # Check if Already Exists
+    $scriptBlock = [Scriptblock]::Create($CheckInstallString)
+    $exist = Start-Job -ScriptBlock $scriptBlock
+    $this_is_to_stop_the_object_being_put_to_the_output_stream = Wait-Job $exist; # Powershell has weird return semantics.
+    $exists = Receive-Job $exist
+    Logger -Message "Package: $Package"
+
+    if(!$exists.Contains($NotInstalledString)){
+        # No Install Job required, send Null Command
+        $mes = "`tAlready installed, skipping."
+        Logger -Message $mes
+        return "$null"
+    }
+    else{
+        # Install
+        $InstallString = "winget install " + $QueryString
+        $message = "`tCommand: " + $InstallString
+        
+        # Log and Send Installer Job
+        Logger -Message $message
+        return $InstallString
+    }
+}
+
+function Install-From-Hashes {
+}
+
+#############################
+# Install Required Programs #
+#############################
+
+Logger -Message "Installing Required Programs.. "
 
 $Jobs = foreach($Package in $MainPrograms.Keys){   
-    $blockstring = Install-Package-ScriptBlock -Package $Package -Source $MainPrograms[$Package] -OtherParameters $DefaultOptionsHash
-    $scriptBlock = [Scriptblock]::Create($blockstring)
     
+    if($PackageParams[$Package] -eq $null) {
+        $blockstring = Install-Package-ScriptBlock `
+            -Package $Package `
+            -Source $MainPrograms[$Package] `
+            -OtherParameters $DefaultOptionsHash;
+    }
+    else {
+        $blockstring = Install-Package-ScriptBlock `
+            -Package $Package `
+            -Source $MainPrograms[$Package] `
+            -PackageParams $PackageParams[$Package] `
+            -OtherParameters $DefaultOptionsHash;
+    }
+
+    $scriptBlock = [Scriptblock]::Create($blockstring);
     Start-Job -ScriptBlock $scriptBlock
 }
 
-Wait-Job $Jobs
-$Output = Receive-Job $Jobs
-foreach ($item in $Output){
-    if($item.Contains("Successfully installed")){
+# Wait For all Install Jobs to Finish
+Logger -Message "`r`nResult Log: "
+$tisofbpttop = Wait-Job $Jobs
+
+foreach ($Job in $Jobs){
+    $Output = Receive-Job $Job
+    if(($Output -ne $null) -and ($Output -is [Array])){
+        $Output = $Output[-1];
+    }
+    
+    if($Output.Contains("Successfully installed")){
         $ResultLog = "Successful"
     }
     else {
-        if($item.Contains("skipping")){
+        if($Output.Contains("skipping")){
             $ResultLog = $item
         }
         else {
             $ResultLog = "Failed"
         }
     }
-    $ResultLog | Out-File -FilePath $Log -Append
+    Logger -Mesage $ResultLog
 }
 
 <#
@@ -105,21 +161,5 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 # Python Library installs
 pip install numpy scipy python-dotenv psycopg2 nuitka
-
-#######################################
-# Not Very Important, yet recommended #
-#######################################
-
-winget install --accept-source-agreements --accept-package-agreements `
-"7-Zip" -s winget
-
-winget install --accept-source-agreements --accept-package-agreements `
-libreoffice -s winget --id TheDocumentFoundation.LibreOffice
-
-winget install --accept-source-agreements --accept-package-agreements `
-ModernFlyouts -s winget
-
-winget install --accept-source-agreements --accept-package-agreements `
-"geogebra classic" -s winget --id "Geogebra.Classic"
 
 #>
